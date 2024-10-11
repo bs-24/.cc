e01030cbe22e172b925b265e683291433fde3726
cha80537 <cha80537@gmail.com>
2024-10-11 15:27:35 +0900

    v0.94

--- main.go
+++ main.go
@@ -5,16 +5,26 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
 	in := bufio.NewReader(os.Stdin)
-	fmt.Print("Input your name : ")
-	name, err := in.ReadString('\n')
+	fmt.Print("Input your score : ")
+	i, err := in.ReadString('\n')
 	if err != nil {
 		log.Fatal(err)
+	}
+	i = strings.TrimSpace(i)                //줄바꿈 제거
+	score, _ := strconv.ParseInt(i, 10, 32) //정수형 10진수 변환
+	if score >= 60 {
+		fmt.Println("A")
+		//status := "passing"
+
 	} else {
-		fmt.Println(name)
+		//status := "failing"
+		fmt.Println("bcdf")
 	}
 
 }
